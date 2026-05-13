56979165d27aab73e419da742726e738f3032b96
hajalosman73-art <hajalosman73@gmail.com>
2026-05-13 21:33:24 +0200

    Update soccer.lua

--- soccer.lua
+++ soccer.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/rblxscriptsnet/rdx/main/scripts/SoccerZero.lua](https://raw.githubusercontent.com/rblxscriptsnet/rdx/main/scripts/SoccerZero.lua
+https://raw.githubusercontent.com/rblxscriptsnet/rdx/main/scripts/SoccerZero.lua
